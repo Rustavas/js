b1d5295d79f148ca1140f24e5ee869775be11225
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-02-08 16:29:53 +0200

    1

--- lesson-01/js/index.js
+++ lesson-01/js/index.js
@@ -1,7 +1,20 @@
-let productName = "Droid";
-let pricePerItem = 2000;
+const productName = "Repair droid";
+console.log(productName[0]); // "R"
+console.log(productName[5]); // "r"
+console.log(productName[11]); // "d"
+console.log(productName[productName.length - 1]); // "d"
 
-// Change code below this line
-pricePerItem = pricePerItem + 500;
-productName = "Repair droid"
-console.log(pricePerItem);
+
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+  if(message.toLowerCase().includes("spam", "sale") {
+    result = true
+  } else {
+    result = false;
+  }
+  
+  // Change code above this line
+  return result;
+}
+ 

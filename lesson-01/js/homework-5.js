1323d5c45d948791e1e03d46d94b8533b06c3167
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-03-04 22:50:19 +0200

    1

--- lesson-01/js/homework-5.js
+++ lesson-01/js/homework-5.js
@@ -83,17 +83,421 @@
 
 // ?=============
 // 4/20
-const users = {
-  name: 'poly',
-}
-const user = {
-  tag: "Mango",
-  showTeg() {
-    console.log('showTeg -> this');
-    console.log('showTeg -> this.tag');
-  },
-};
-user.showTeg();
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
-const outerShowTeg = user.showTeg();
-outerShowTeg();
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+// ?=============
+// 5/20
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child.heritage, child.name);
+
+// ?=============
+// 7/20
+// class Car {
+//  constructor(brand, model, price) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+//  };
+
+  // Change code above this line
+// };
+// const car1 = new Car("Audi", "Q3", 36000);
+// console.log(car1);
+
+// ?=============
+// 8/20
+// class Car {
+//   // Change code below this line
+//   constructor({brand, model, price}) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
+// ?=============
+// 9/20
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice(){
+//     return this.price
+//   }
+//   changePrice(newPrice){
+//   this.price = newPrice
+//   }  
+//   // Change code below this line
+//   // Change code above this line
+// }
+
+// ?=============
+// 10/20
+// class Storage {
+  // constructor(items){
+  //   this.items = items;
+  // };
+  
+  // getItems(){
+  //  return this.items;
+  // };
+  // addItem(newItem){
+  //   this.items.push(newItem);
+  // };
+  // removeItem(itemToRemove){
+  //   return this.items.splice(this.items.indexOf(itemToRemove), 1); 
+
+  //! removeItem(itemToRemove){
+  //!   return this.items.filter(item => item !== itemToRemove);  
+// };
+// };
+// Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// ?=============
+// 11/20
+// class StringBuilder {
+//   constructor(initialValue){
+//     this.value = initialValue;
+//   };
+//   getValue(){
+//     return this.value;
+//   };
+//   padEnd(str){
+//      this.value = this.value + str;
+//   };
+//   padStart(str){
+//      this.value = str + this.value;
+//   };
+//   padBoth(str){
+//      this.value = str + this.value + str;
+//   };
+// };
+
+// Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); 
+
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// ?=============
+// 12/20
+// class Car {
+//   // Change code below this line
+// #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// getBrand(){
+//   return this.#brand;
+// };
+//   changeBrand(newBrand){
+//     this.#brand = newBrand;
+//   };
+//   // Change code above this line
+// }
+
+// ?=============
+// 13/20
+// class Storage {
+//   // Change code below this line
+// #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+// ?=============
+// 14/20
+// class StringBuilder {
+//   // Change code below this line
+// #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// ?=============
+// 15/20
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   #model;
+//   #price;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+//   get brand() {
+//     return this.#brand;
+//   }
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+//   get model() {
+//     return this.#model;
+//   }
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+// ?=============
+// 16/20
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (Car.MAX_PRICE >= newPrice){
+//       this.#price = newPrice;
+//     }
+   
+//     return this.#price;
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// ?=============
+// 17/20
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+// static checkPrice(price){
+//   if(price > Car.#MAX_PRICE){
+//    return "Error! Price exceeds the maximum"; 
+//   }
+//   return "Success! Price is within acceptable limits"
+// }
+  // Change code above this line
+  // constructor({ price }) {
+  //   this.price = price;
+  // }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// ?=============
+// 18/20
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
+// class Admin extends User{
+//   static AccessLevel = { BASIC: "basic", SUPERUSER: "superuser" };
+// }
+
+// ?=============
+// 19/20
+// class User {
+//   email;
+//   constructor(email) {
+//     this.email = email;
+//   }
+//   get email() {
+//     return this.email;
+//   }
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+// constructor({email, accessLevel}){
+//  super(email);
+// }
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// ?=============
+// 20/20
+// class User {
+//   email;
+//   constructor(email) {
+//     this.email = email;
+//   }
+//   get email() {
+//     return this.email;
+//   }
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+//   blacklistedEmails;
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+    
+//   }
+//   blacklistedEmails = []; 
+
+//   blacklist(email){
+//  this.blacklistedEmails.push(email); 
+//   }
+//   isBlacklisted(email){
+//    return this.blacklistedEmails.includes(email);
+//   }
+//   // Change code above this line
+// }
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
